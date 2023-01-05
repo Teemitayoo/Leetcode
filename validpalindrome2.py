c2028cf42d87b2720657a70f5b797c884f7cd447
Temitayo Akinsanya <57836785+Teemitayoo@users.noreply.github.com>
2023-01-05 23:49:58 +0100

    Create validpalindrome2.py

---
+++ validpalindrome2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        i,j=0,len(s)-1
+        while i<j:
+            if s[i]!=s[j]:
+                left_remove=s[i+1:j+1]
+                right_remove=s[i:j]
+                return left_remove==left_remove[-1::-1] or right_remove==right_remove[-1::-1]
+            else:
+                i+=1
+                j-=1
+        return True
